3372034f45ff9d08d9843fbd3f91fcff1e028d10
Nikita Golda <kitagolda@gmail.com>
2020-09-02 23:35:43 +0300

    Very old html/css practice

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg"></button>',
+        autoplay: true,
+        autospeed: 6000,
+        fade: true
+    });
+});
+
+// $(function(){
+//     $('.our-trip__slider').slick({
+//         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg"></button>',
+        // autoplay: true,
+//         autospeed: 6000,
+//         fade: true
+//     });
+// });
